4ab5c714e12c90cc0c097acde71bbe3e35f12885
vikas-0 <vikas@geekyhub.in>
2017-09-27 00:29:14 +0530

    better welcome page B)

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './css/style.css';
+import emoji from './images/Smiling-Face-with-Sunglasses-Cool-Emoji-PNG.png';
 
 ReactDOM.render(
-    <h1>
-        Hello world
-    </h1>, 
+    <div>
+        <img src={emoji}/>
+        <h1>Congrats!</h1>
+        <p>Now go to &apos;src&apos; folder and add/edit codes</p>
+    </div>, 
     document.getElementById('root')
 );
